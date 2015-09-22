0de09983a4750facfc276c95fdd8a334e33c236b
Peter Maselkowski <pmaselkowski@gmail.com>
2015-09-22 19:57:16 +0200

    - Removed lightbox related code + Added css class to gallery

--- src/Widgets/views/gallery.php
+++ src/Widgets/views/gallery.php
@@ -7,9 +7,9 @@ use Maslosoft\Staple\Widgets\Vo\GalleryFile;
 /* @var $this Gallery */
 /* @var $file GalleryFile */
 ?>
-<div style="margin: 0px auto;">
+<div style="margin: 0px auto;" class="staple-gallery">
 	<?php foreach ($this->getFiles() as $file): ?>
-		<a href="<?= $file->getUrl(); ?>" data-lightbox="gallery" class="img-lightbox">
+		<a href="<?= $file->getUrl(); ?>" style="cursor:zoom-in;">
 			<img src="<?= $file->getThumbUrl(); ?>"  class="img-thumbnail img-margins" style="vertical-align: top;"/>
 		</a>
 	<?php endforeach; ?>
